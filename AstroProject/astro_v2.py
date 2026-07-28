966fd60e8f98b71b73336f597bc1879008c77046
Folombas <folombas@yandex.ru>
2026-07-28 17:13:33 +0000

    feat: reorganize project into AstroProject folder and add Eastern zodiac logic

---
+++ AstroProject/astro_v2.py
@@ -0,0 +1,5 @@
+# Astro-Financial Engine v2.0
+
+def get_eastern_zodiac(year):
+    animals = ['Крыса', 'Бык', 'Тигр', 'Кролик', 'Дракон', 'Змея', 'Лошадь', 'Коза', 'Обезьяна', 'Петух', 'Собака', 'Свинья']
+    return animals[(year - 4) % 12]
