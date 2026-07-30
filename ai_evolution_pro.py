0759f86ba64883e4d74f09f4191985768fa0b4a3
Folombas <folombas@yandex.ru>
2026-07-30 18:20:46 +0000

    perf: add bit-precision quantization to module 147

--- ai_evolution_pro.py
+++ ai_evolution_pro.py
@@ -446,3 +446,6 @@ def quantize_145(w): return np.round(w, 4)
 
 # Performance Phase 146: Quantization layer 46
 def quantize_146(w): return np.round(w, 3)
+
+# Performance Phase 147: Quantization layer 47
+def quantize_147(w): return np.round(w, 4)
