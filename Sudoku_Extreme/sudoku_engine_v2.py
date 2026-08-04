917c34eef00fb0818012a65bfba94efde26f0a75
Folombas <folombas@yandex.ru>
2026-08-04 20:05:49 +0000

    refactor: recursive backtracking solver - logic 156

--- Sudoku_Extreme/sudoku_engine_v2.py
+++ Sudoku_Extreme/sudoku_engine_v2.py
@@ -159,3 +159,4 @@ import random
 # Milestone 153: Solver heuristics improved 53
 # Milestone 154: Solver heuristics improved 54
 # Milestone 155: Solver heuristics improved 55
+# Milestone 156: Solver heuristics improved 56
