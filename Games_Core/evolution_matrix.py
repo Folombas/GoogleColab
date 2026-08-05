935bfc077a913dfc775388226e2c0448c5c63f49
Folombas <folombas@yandex.ru>
2026-08-05 19:22:06 +0000

    feat: achievement and reward scaling - tier 55

--- Games_Core/evolution_matrix.py
+++ Games_Core/evolution_matrix.py
@@ -255,3 +255,4 @@
 # Milestone 252: Unlockable medal 52 integrated
 # Milestone 253: Unlockable medal 53 integrated
 # Milestone 254: Unlockable medal 54 integrated
+# Milestone 255: Unlockable medal 55 integrated
