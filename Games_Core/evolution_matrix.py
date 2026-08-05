6f15d68200c503e0d76da4db591102687040f984
Folombas <folombas@yandex.ru>
2026-08-05 19:22:08 +0000

    perf: global latency reduction - final patch 31

--- Games_Core/evolution_matrix.py
+++ Games_Core/evolution_matrix.py
@@ -331,3 +331,4 @@
 # Milestone 328: Final release stabilization 28
 # Milestone 329: Final release stabilization 29
 # Milestone 330: Final release stabilization 30
+# Milestone 331: Final release stabilization 31
