37a9b4b41d4719b751afc07906d939290da647d9
Folombas <folombas@yandex.ru>
2026-08-05 19:22:05 +0000

    feat: achievement and reward scaling - tier 14

--- Games_Core/evolution_matrix.py
+++ Games_Core/evolution_matrix.py
@@ -214,3 +214,4 @@
 # Milestone 211: Unlockable medal 11 integrated
 # Milestone 212: Unlockable medal 12 integrated
 # Milestone 213: Unlockable medal 13 integrated
+# Milestone 214: Unlockable medal 14 integrated
