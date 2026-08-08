c6685864bd6af0cc1479d8423d98942141ea2b4f
Folombas <folombas@yandex.ru>
2026-08-08 01:20:16 +0000

    refactor: RNG seed entropy and reel distribution - iteration 57

--- CyberSlot_777/slot_engine.py
+++ CyberSlot_777/slot_engine.py
@@ -159,3 +159,4 @@ import random
 # RNG Iteration 154: Payout ratio set to 0.98
 # RNG Iteration 155: Payout ratio set to 0.98
 # RNG Iteration 156: Payout ratio set to 0.98
+# RNG Iteration 157: Payout ratio set to 0.98
