8fbc72908d901c5742fe19ae9943e638bb30a23b
Folombas <folombas@yandex.ru>
2026-08-08 01:20:15 +0000

    feat: lever physics and spring dynamics - step 37

--- CyberSlot_777/slot_engine.py
+++ CyberSlot_777/slot_engine.py
@@ -39,3 +39,4 @@ import random
 # Lever Step 34: Drag resistance optimized
 # Lever Step 35: Drag resistance optimized
 # Lever Step 36: Drag resistance optimized
+# Lever Step 37: Drag resistance optimized
